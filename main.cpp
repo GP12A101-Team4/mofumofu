8ea881cdc428edf5244d43e25bb4b15e5de8f110
INADON <inari0720@outlook.com>
2025-05-21 15:07:04 +0900

    ver 0.1

---
+++ main.cpp
@@ -0,0 +1,534 @@
+﻿//=============================================================================
+//
+// メイン処理 [main.cpp]
+// Author : 
+//
+//=============================================================================
+#include "main.h"
+#include "renderer.h"
+#include "input.h"
+#include "camera.h"
+#include "debugproc.h"
+#include "model.h"
+#include "player.h"
+#include "shadow.h"
+#include "light.h"
+#include "meshfield.h"
+#include "meshwall.h"
+#include "sound.h"
+#include "score.h"
+#include "sprite.h"
+
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define CLASS_NAME		"AppClass"			// ウインドウのクラス名
+#define WINDOW_NAME		"メッシュ表示"		// ウインドウのキャプション名
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+HRESULT Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow);
+void Uninit(void);
+void Update(void);
+void Draw(void);
+
+//*****************************************************************************
+// グローバル変数:
+//*****************************************************************************
+long g_MouseX = 0;
+long g_MouseY = 0;
+
+HWND g_hWnd = nullptr;
+
+float yaw ;
+float pitch;
+
+#ifdef _DEBUG
+int		g_CountFPS;							// FPSカウンタ
+char	g_DebugStr[2048] = WINDOW_NAME;		// デバッグ文字表示用
+
+#endif
+
+
+//=============================================================================
+// メイン関数
+//=============================================================================
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);	// 無くても良いけど、警告が出る（未使用宣言）
+	UNREFERENCED_PARAMETER(lpCmdLine);		// 無くても良いけど、警告が出る（未使用宣言）
+
+	// 時間計測用
+	DWORD dwExecLastTime;
+	DWORD dwFPSLastTime;
+	DWORD dwCurrentTime;
+	DWORD dwFrameCount;
+
+	WNDCLASSEX	wcex = {
+		sizeof(WNDCLASSEX),
+		CS_CLASSDC,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		NULL,
+		LoadCursor(NULL, IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		NULL,
+		CLASS_NAME,
+		NULL
+	};
+	HWND		hWnd;
+	MSG			msg;
+	
+	// ウィンドウクラスの登録
+	RegisterClassEx(&wcex);
+
+	// ウィンドウの作成
+	hWnd = CreateWindow(CLASS_NAME,
+		WINDOW_NAME,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,																		// ウィンドウの左座標
+		CW_USEDEFAULT,																		// ウィンドウの上座標
+		SCREEN_WIDTH + GetSystemMetrics(SM_CXDLGFRAME) * 2,									// ウィンドウ横幅
+		SCREEN_HEIGHT + GetSystemMetrics(SM_CXDLGFRAME) * 2 + GetSystemMetrics(SM_CYCAPTION),	// ウィンドウ縦幅
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	// 初期化処理(ウィンドウを作成してから行う)
+	if(FAILED(Init(hInstance, hWnd, TRUE)))
+	{
+		return -1;
+	}
+
+	// フレームカウント初期化
+	timeBeginPeriod(1);	// 分解能を設定
+	dwExecLastTime = dwFPSLastTime = timeGetTime();	// システム時刻をミリ秒単位で取得
+	dwCurrentTime = dwFrameCount = 0;
+
+	// ウインドウの表示(初期化処理の後に呼ばないと駄目)
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	
+	// メッセージループ
+	while(1)
+	{
+		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if(msg.message == WM_QUIT)
+			{// PostQuitMessage()が呼ばれたらループ終了
+				break;
+			}
+			else
+			{
+				// メッセージの翻訳と送出
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+        }
+		else
+		{
+			dwCurrentTime = timeGetTime();
+
+			if ((dwCurrentTime - dwFPSLastTime) >= 1000)	// 1秒ごとに実行
+			{
+#ifdef _DEBUG
+				g_CountFPS = dwFrameCount;
+#endif
+				dwFPSLastTime = dwCurrentTime;				// FPSを測定した時刻を保存
+				dwFrameCount = 0;							// カウントをクリア
+			}
+
+			if ((dwCurrentTime - dwExecLastTime) >= (1000 / 60))	// 1/60秒ごとに実行
+			{
+				dwExecLastTime = dwCurrentTime;	// 処理した時刻を保存
+
+#ifdef _DEBUG	// デバッグ版の時だけFPSを表示する
+				wsprintf(g_DebugStr, WINDOW_NAME);
+				wsprintf(&g_DebugStr[strlen(g_DebugStr)], " FPS:%d", g_CountFPS);
+#endif
+
+				Update();			// 更新処理
+				Draw();				// 描画処理
+
+#ifdef _DEBUG	// デバッグ版の時だけ表示する
+				wsprintf(&g_DebugStr[strlen(g_DebugStr)], " MX:%d MY:%d", GetMousePosX(), GetMousePosY());
+				SetWindowText(hWnd, g_DebugStr);
+#endif
+
+				dwFrameCount++;
+			}
+		}
+	}
+
+	timeEndPeriod(1);				// 分解能を戻す
+
+	// ウィンドウクラスの登録を解除
+	UnregisterClass(CLASS_NAME, wcex.hInstance);
+
+	// 終了処理
+	Uninit();
+
+	return (int)msg.wParam;
+}
+
+//=============================================================================
+// プロシージャ
+//=============================================================================
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UINT dataSize = 0; 
+	switch (message)
+	{
+	case WM_CREATE:
+		g_hWnd = hWnd;
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hWnd);
+			break;
+		}
+		break;
+
+	case WM_MOUSEMOVE:
+		g_MouseX = LOWORD(lParam);
+		g_MouseY = HIWORD(lParam);
+
+		POINT point = GetClientCenter(g_hWnd);
+		SetCursorPos(point.x, point.y);
+
+
+		break;
+
+	case WM_ACTIVATE:
+		if (wParam != WA_INACTIVE) {
+
+			ShowCursor(FALSE);
+			RECT rect;
+			GetClientRect(hWnd, &rect);
+			MapWindowPoints(hWnd, nullptr, (POINT*)&rect, 2);
+			ClipCursor(&rect);
+
+		}
+		else {
+
+		/*	ClipCursor(nullptr);*/
+			ShowCursor(TRUE);
+		}
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT Init(HINSTANCE hInstance, HWND hWnd, BOOL bWindow)
+{
+	// レンダラーの初期化
+	InitRenderer(hInstance, hWnd, bWindow);
+
+	// ライトの初期化
+	InitLight();
+
+	// カメラの初期化
+	InitCamera();
+
+	////サウンドの初期化
+	InitSound(hWnd);
+
+	InitScore();
+
+	// 入力処理の初期化
+	InitInput(hInstance, hWnd);
+
+	// フィールドの初期化
+	InitMeshField(XMFLOAT3(0.0f, 0.0f, 0.0f), XMFLOAT3(0.0f, 0.0f, 0.0f), 100, 100, 13.0f, 13.0f);
+
+	// 壁の初期化
+	InitMeshWall(XMFLOAT3(0.0f, 0.0f, MAP_TOP), XMFLOAT3(0.0f, 0.0f, 0.0f),
+		XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
+	InitMeshWall(XMFLOAT3(MAP_LEFT, 0.0f, 0.0f), XMFLOAT3(0.0f, -XM_PI * 0.50f, 0.0f),
+		XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
+	InitMeshWall(XMFLOAT3(MAP_RIGHT, 0.0f, 0.0f), XMFLOAT3(0.0f, XM_PI * 0.50f, 0.0f),
+		XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
+	InitMeshWall(XMFLOAT3(0.0f, 0.0f, MAP_DOWN), XMFLOAT3(0.0f, XM_PI, 0.0f),
+		XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
+
+	// 壁(裏側用の半透明)
+	InitMeshWall(XMFLOAT3(0.0f, 0.0f, MAP_TOP), XMFLOAT3(0.0f, XM_PI, 0.0f),
+		XMFLOAT4(1.0f, 1.0f, 1.0f, 0.25f), 16, 2, 80.0f, 80.0f);
+	InitMeshWall(XMFLOAT3(MAP_LEFT, 0.0f, 0.0f), XMFLOAT3(0.0f, XM_PI * 0.50f, 0.0f),
+		XMFLOAT4(1.0f, 1.0f, 1.0f, 0.25f), 16, 2, 80.0f, 80.0f);
+	InitMeshWall(XMFLOAT3(MAP_RIGHT, 0.0f, 0.0f), XMFLOAT3(0.0f, -XM_PI * 0.50f, 0.0f),
+		XMFLOAT4(1.0f, 1.0f, 1.0f, 0.25f), 16, 2, 80.0f, 80.0f);
+	InitMeshWall(XMFLOAT3(0.0f, 0.0f, MAP_DOWN), XMFLOAT3(0.0f, 0.0f, 0.0f),
+		XMFLOAT4(1.0f, 1.0f, 1.0f, 0.25f), 16, 2, 80.0f, 80.0f);
+
+	// 影の初期化処理
+	InitShadow();
+
+	// プレイヤーの初期化
+	InitPlayer();
+
+	// ライトを有効化
+	SetLightEnable(FALSE);
+
+	// 背面ポリゴンをカリング
+	SetCullingMode(CULL_MODE_BACK);
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void Uninit(void)
+{
+
+	// プレイヤーの終了処理
+	UninitPlayer();
+
+	// 影の終了処理
+	UninitShadow();
+
+	// 壁の終了処理
+	UninitMeshWall();
+
+	// 地面の終了処理
+	UninitMeshField();
+
+	// カメラの終了処理
+	UninitCamera();
+
+	////サウンドの終了処理
+	UninitSound();
+
+	UninitScore();
+
+	//入力の終了処理
+	UninitInput();
+
+	// レンダラーの終了処理
+	UninitRenderer();
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void Update(void)
+{
+	// 入力の更新処理
+	UpdateInput();
+
+	// プレイヤーの更新処理
+	UpdatePlayer();
+
+	// カメラ更新
+	UpdateCamera();
+
+	// 地面処理の更新
+	UpdateMeshField();
+
+
+	// 壁処理の更新
+	UpdateMeshWall();
+
+	// 影の更新処理
+	UpdateShadow();
+
+	UpdateScore();
+
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void Draw0(void) {
+	// 地面の描画処理
+	DrawMeshField();
+
+	// 影の描画処理
+	DrawShadow();
+
+	// プレイヤーの描画処理
+	DrawPlayer();
+
+	// 壁の描画処理
+	DrawMeshWall();
+
+}
+
+void Draw(void)
+{
+	// バックバッファクリア
+	Clear();
+
+	CAMERA* camera = GetCamera();
+	PLAYER* player = GetPlayer();
+
+	{
+		
+
+		camera->dir = GetCameraDir(player->pos);
+		
+		SetCameraAT(camera->dir);
+		SetCamera();
+		
+
+
+		SetViewPort(TYPE_FULL_SCREEN);
+		Draw0();	//OBJ描画処理
+	}
+
+	//すべての3D
+
+	// 2Dの物を描画する処理
+	SetViewPort(TYPE_FULL_SCREEN);
+
+	// Z比較なし
+	SetDepthEnable(FALSE);
+
+	// ライティングを無効
+	SetLightEnable(FALSE);
+
+	//ここに2Dで表示させたいものを書いていく
+
+	DrawScore();	//スコア描画
+
+	// Z比較なし
+	SetDepthEnable(TRUE);
+
+	// ライティングを無効
+	SetLightEnable(TRUE);
+
+
+
+	
+
+
+#ifdef _DEBUG
+	// デバッグ表示
+	DrawDebugProc();
+#endif
+
+	// バックバッファ、フロントバッファ入れ替え
+	Present();
+}
+
+POINT GetClientCenter(HWND hWnd){
+	RECT clientRect;
+	GetClientRect(hWnd, &clientRect);
+	int centerX = (clientRect.left + clientRect.right) / 2;
+	int centerY = (clientRect.top + clientRect.bottom) / 2;
+
+	POINT Center = { centerX, centerY };
+	ClientToScreen(hWnd, &Center);
+
+	return Center;
+}
+POINT GetClientCursorPos(HWND hWnd) {
+	POINT cursor;
+	GetCursorPos(&cursor);
+
+	return cursor;
+}
+
+XMFLOAT3 GetCameraDir(XMFLOAT3 pos) {
+	/*POINT center = GetClientCenter(g_hWnd);
+	POINT cursor = GetClientCursorPos(g_hWnd);*/
+
+	int deltaX = GetMouseX();
+	int deltaZ = GetMouseY();
+
+	XMFLOAT3 dir;
+
+	yaw   += deltaX * 0.4f;
+	pitch -= deltaZ * 0.4f;	
+
+	if (pitch > 89.0f) { pitch = 89.0f; }
+	if (pitch < -89.0f) { pitch = -89.0f; }
+
+	float yawRad = DirectX::XMConvertToRadians(yaw);
+	float pitchRad = DirectX::XMConvertToRadians(pitch);
+
+	dir = { pos.x + sinf(yawRad) * cosf((pitchRad)),pos.y + sinf(pitchRad), pos.z + cosf(yawRad) * cosf(pitchRad) };
+
+	return dir;
+}
+
+float GetCameraYaw(XMFLOAT3 dir, XMFLOAT3 pos) {
+	float deltaX = dir.x - pos.x;
+	float deltaZ = dir.z - pos.z;
+
+	float yaw = atan2f(deltaX, deltaZ);
+
+	return yaw;
+}
+
+float GetCameraPitch(XMFLOAT3 dir, XMFLOAT3 pos) {
+	float deltaY = dir.y - pos.y;
+	float deltaX = dir.x - pos.x;
+	float deltaZ = dir.z - pos.z;
+
+	float XY = (float)sqrt(deltaX * deltaX + deltaZ * deltaZ);
+
+	float pitch = atan2f(deltaY, XY);
+
+		
+
+	return pitch;
+}
+
+XMFLOAT3 GetDeltaMove(XMFLOAT3 dir, XMFLOAT3 pos) {
+	XMFLOAT3 DeltaMove;
+	DeltaMove.x = dir.x - pos.x;
+	DeltaMove.y = dir.y - pos.y;
+	DeltaMove.z = dir.z - pos.z;
+
+	return DeltaMove;
+}
+
+
+
+long GetMousePosX(void)
+{
+	return g_MouseX;
+}
+
+
+long GetMousePosY(void)
+{
+	return g_MouseY;
+}
+
+
+
+void HandleMouseMove(int deltaX, int deltaY) {
+	float sensitivity = 0.1f; // 感度設定
+	float yaw = deltaX * sensitivity;  // 水平回転（Yaw）
+	float pitch = deltaY * sensitivity; // 垂直回転（Pitch）
+}
+
+
+#ifdef _DEBUG
+char* GetDebugStr(void)
+{
+	return g_DebugStr;
+}
+#endif
---
+++ player.cpp
@@ -0,0 +1,227 @@
+//=============================================================================
+//
+// モデル処理 [player.cpp]
+// Author : 
+//
+//=============================================================================
+#include "main.h"
+#include "input.h"
+#include "camera.h"
+#include "debugproc.h"
+#include "model.h"
+#include "player.h"
+#include "shadow.h"
+#include "light.h"
+#include "collision.h"
+#include "sound.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define	MODEL_PLAYER		"data/MODEL/player.obj"			// 読み込むモデル名
+
+#define	VALUE_MOVE			(2.0f)							// 移動量
+#define	VALUE_ROTATE		(XM_PI * 0.02f)					// 回転量
+
+#define PLAYER_SHADOW_SIZE	(0.4f)							// 影の大きさ
+#define PLAYER_OFFSET_Y		(7.0f)							// プレイヤーの足元をあわせる
+
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+
+
+//*****************************************************************************
+// グローバル変数
+//*****************************************************************************
+static PLAYER				g_Player;						// プレイヤー
+
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT InitPlayer(void)
+{
+	LoadModel(MODEL_PLAYER, &g_Player.model);
+	g_Player.load = TRUE;
+
+	g_Player.pos = { 0.0f, PLAYER_OFFSET_Y, 0.0f };
+	g_Player.rot = { 0.0f, 0.0f, 0.0f };
+	g_Player.scl = { 1.0f, 1.0f, 1.0f };
+
+	g_Player.spd = 0.0f;			// 移動スピードクリア
+	g_Player.size = PLAYER_SIZE;	// 当たり判定の大きさ
+
+	g_Player.ammor = 0.0f;
+
+	g_Player.use = TRUE;
+
+	// ここでプレイヤー用の影を作成している
+	XMFLOAT3 pos = g_Player.pos;
+	pos.y -= (PLAYER_OFFSET_Y - 0.1f);
+	g_Player.shadowIdx = CreateShadow(pos, PLAYER_SHADOW_SIZE, PLAYER_SHADOW_SIZE);
+	//          ↑
+	//        このメンバー変数が生成した影のIndex番号
+
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void UninitPlayer(void)
+{
+	// モデルの解放処理
+	if (g_Player.load)
+	{
+		UnloadModel(&g_Player.model);
+		g_Player.load = FALSE;
+	}
+
+
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void UpdatePlayer(void)
+{
+	CAMERA *cam = GetCamera();
+
+	// 移動させちゃう
+	if (GetKeyboardPress(DIK_A))
+	{	// 左へ移動
+		g_Player.spd = VALUE_MOVE;
+		g_Player.dir = XM_PI / 2;
+	}
+	if (GetKeyboardPress(DIK_D))
+	{	// 右へ移動
+		g_Player.spd = VALUE_MOVE;
+		g_Player.dir = -XM_PI / 2;
+	}
+	if (GetKeyboardPress(DIK_W))
+	{	// 上へ移動
+		g_Player.spd = VALUE_MOVE;
+		g_Player.dir = XM_PI;
+	}
+	if (GetKeyboardPress(DIK_S))
+	{	// 下へ移動
+		g_Player.spd = VALUE_MOVE;
+		g_Player.dir = 0.0f;
+	}
+	
+	
+#ifdef _DEBUG
+	if (GetKeyboardPress(DIK_R))
+	{
+		g_Player.pos.z = g_Player.pos.x = 0.0f;
+		g_Player.rot.y = g_Player.dir = 0.0f;
+		g_Player.spd = 0.0f;
+	}
+#endif
+
+	
+
+	CAMERA* camera = GetCamera();
+	g_Player.rot.y = GetCameraYaw(camera->dir, camera->pos)+ g_Player.dir;
+	//	// Key入力があったら移動処理する
+	if (g_Player.spd != 0.0f)
+	{
+		float moveX = -sinf(g_Player.rot.y) * g_Player.spd;
+		float moveY = -cosf(g_Player.rot.y) * g_Player.spd;
+		
+
+		// 入力のあった方向へプレイヤーを向かせて移動させる
+		g_Player.pos.x += moveX;
+		g_Player.pos.z += moveY;
+
+		
+	}
+
+	// 影もプレイヤーの位置に合わせる
+	XMFLOAT3 pos = g_Player.pos;
+	pos.y -= (PLAYER_OFFSET_Y - 0.1f);
+	SetPositionShadow(g_Player.shadowIdx, pos);
+
+
+	g_Player.spd *= 0.5f;
+
+
+
+	//{	// ポイントライトのテスト
+	//	LIGHT *light = GetLightData(1);
+	//	XMFLOAT3 pos = g_Player.pos;
+	//	pos.y += 20.0f;
+
+	//	light->Position = pos;
+	//	light->Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	//	light->Ambient = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	//	light->Type = LIGHT_TYPE_POINT;
+	//	light->Enable = TRUE;
+	//	SetLightData(1, light);
+	//}
+
+	long X = GetMouseX();
+	long Y = GetMouseY();
+	long Z = GetMouseZ();
+
+	
+
+#ifdef _DEBUG	// デバッグ情報を表示する
+	PrintDebugProc("Player:↑ → ↓ ←　Space\n");
+	PrintDebugProc("Player:X:%f Y:%f Z:%f\n", g_Player.pos.x, g_Player.pos.y, g_Player.pos.z);
+	PrintDebugProc("X:%d Y:%d Z:%d \n", X, Y, Z);
+#endif
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void DrawPlayer(void)
+{
+	// カリング無効
+	SetCullingMode(CULL_MODE_NONE);
+
+	XMMATRIX mtxScl, mtxRot, mtxTranslate, mtxWorld;
+
+	// ワールドマトリックスの初期化
+	mtxWorld = XMMatrixIdentity();
+
+	// スケールを反映
+	mtxScl = XMMatrixScaling(g_Player.scl.x, g_Player.scl.y, g_Player.scl.z);
+	mtxWorld = XMMatrixMultiply(mtxWorld, mtxScl);
+
+	// 回転を反映
+	mtxRot = XMMatrixRotationRollPitchYaw(g_Player.rot.x, g_Player.rot.y + XM_PI, g_Player.rot.z);
+	mtxWorld = XMMatrixMultiply(mtxWorld, mtxRot);
+
+	// 移動を反映
+	mtxTranslate = XMMatrixTranslation(g_Player.pos.x, g_Player.pos.y, g_Player.pos.z);
+	mtxWorld = XMMatrixMultiply(mtxWorld, mtxTranslate);
+
+	// ワールドマトリックスの設定
+	SetWorldMatrix(&mtxWorld);
+
+	XMStoreFloat4x4(&g_Player.mtxWorld, mtxWorld);
+
+
+	// モデル描画
+	DrawModel(&g_Player.model);
+
+	// カリング設定を戻す
+	SetCullingMode(CULL_MODE_BACK);
+}
+
+
+//=============================================================================
+// プレイヤー情報を取得
+//=============================================================================
+PLAYER *GetPlayer(void)
+{
+	return &g_Player;
+}
+
+
+
