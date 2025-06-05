f4387facb3acf2a94a52c78a6482101adc38ba7e
GPLiuMing <hal.ryumei.2026@gmail.com>
2025-06-05 14:48:00 +0900

    触发判定条件后的完整图像描画
    触发判定条件后的完整图像描画

--- fragment.cpp
+++ fragment.cpp
@@ -14,7 +14,7 @@
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
-#define TEXTURE_MAX		(4)						// テクスチャの数
+#define TEXTURE_MAX		(5)						// テクスチャの数
 
 #define MAX_POLYGON		(6)						// キューブ１個あたりの面数
 
@@ -28,6 +28,7 @@
 //*****************************************************************************
 HRESULT MakeVertexFragment(void);
 
+bool CheckPuzzleRestored(void);
 
 //*****************************************************************************
 // グローバル変数
@@ -40,13 +41,14 @@ static int							g_TexNo;				// テクスチャ番号
 
 static XMFLOAT2 g_TargetScreenPos[TEXTURE_MAX];  // 记录正确屏幕位置
 static bool g_HasRecordedTarget = false;         // 是否记录过目标位置
+static bool g_ShowFullImage = false;
 
 static char* g_TextureName[] = {
 	"data/TEXTURE/cat_01.png",
 	"data/TEXTURE/cat_02.png",
 	"data/TEXTURE/cat_03.png",
 	"data/TEXTURE/cat_04.png",
-
+	"data/TEXTURE/cat.png",
 };
 
 // 頂点配列
@@ -62,6 +64,41 @@ static VERTEX_3D g_VertexArray[4 * MAX_POLYGON] = {
    
 
 };
+
+XMFLOAT3 ComputePuzzleCenter()
+{
+	XMFLOAT3 center = { 0.0f, 0.0f, 0.0f };
+	for (int i = 0; i < TEXTURE_MAX - 1; i++)  
+	{
+		center.x += g_Fragment[i].overallPos.x;
+		center.y += g_Fragment[i].overallPos.y;
+		center.z += g_Fragment[i].overallPos.z;
+	}
+	center.x /= (TEXTURE_MAX - 1);
+	center.y /= (TEXTURE_MAX - 1);
+	center.z /= (TEXTURE_MAX - 1);
+	return center;
+}
+
+XMFLOAT3 ComputeAverageScale()
+{
+	XMFLOAT3 avg = { 0.0f, 0.0f, 0.0f };
+
+	int count = TEXTURE_MAX - 1;
+
+	for (int i = 0; i < count; i++)
+	{
+		avg.x += g_Fragment[i].scl.x;
+		avg.y += g_Fragment[i].scl.y;
+		avg.z += g_Fragment[i].scl.z;
+	}
+
+	avg.x /= (float)count;
+	avg.y /= (float)count;
+	avg.z = 1.0f;
+
+	return avg;
+}
 
 //=============================================================================
 // 初期化処理
@@ -131,10 +168,18 @@ void UninitFragment(void)
 void UpdateFragment(void)
 {
 	CAMERA *cam = GetCamera();
+	
+	XMFLOAT3 center = ComputePuzzleCenter();
+
+	if (!g_ShowFullImage && CheckPuzzleRestored())
+	{
+		g_ShowFullImage = true;
+		
+	}
 
 #ifdef _DEBUG
 	
-	//猫の頭
+	////猫の頭
 	//if (GetKeyboardPress(DIK_LEFT))
 	//{
 	//	g_Fragment[0].overallPos.x -= 1.0f;
@@ -160,155 +205,181 @@ void UpdateFragment(void)
 	//	g_Fragment[0].overallPos.z += 1.0f;
 	//}
 
-	//猫の体
-	/*if (GetKeyboardPress(DIK_LEFT))
-	{
-		g_Fragment[0].overallPos.x -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_RIGHT))
-	{
-		g_Fragment[0].overallPos.x += 1.0f;
-	}
-	if (GetKeyboardPress(DIK_UP))
-	{
-		g_Fragment[0].overallPos.y += 1.0f;
-	}
-	if (GetKeyboardPress(DIK_DOWN))
-	{
-		g_Fragment[0].overallPos.y -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_M))
-	{
-		g_Fragment[0].overallPos.z -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_N))
-	{
-		g_Fragment[0].overallPos.z += 1.0f;
-	}*/
+	////猫の体
+	//if (GetKeyboardPress(DIK_LEFT))
+	//{
+	//	g_Fragment[0].overallPos.x -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_RIGHT))
+	//{
+	//	g_Fragment[0].overallPos.x += 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_UP))
+	//{
+	//	g_Fragment[0].overallPos.y += 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_DOWN))
+	//{
+	//	g_Fragment[0].overallPos.y -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_M))
+	//{
+	//	g_Fragment[0].overallPos.z -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_N))
+	//{
+	//	g_Fragment[0].overallPos.z += 1.0f;
+	//}
 
-	//猫の手
-	if (GetKeyboardPress(DIK_LEFT))
-	{
-		g_Fragment[2].overallPos.x -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_RIGHT))
-	{
-		g_Fragment[2].overallPos.x += 1.0f;
-	}
-	if (GetKeyboardPress(DIK_UP))
-	{
-		g_Fragment[2].overallPos.y += 1.0f;
-	}
-	if (GetKeyboardPress(DIK_DOWN))
-	{
-		g_Fragment[2].overallPos.y -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_M))
-	{
-		g_Fragment[2].overallPos.z -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_N))
-	{
-		g_Fragment[2].overallPos.z += 1.0f;
-	}
+	////猫の手
+	//if (GetKeyboardPress(DIK_LEFT))
+	//{
+	//	g_Fragment[2].overallPos.x -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_RIGHT))
+	//{
+	//	g_Fragment[2].overallPos.x += 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_UP))
+	//{
+	//	g_Fragment[2].overallPos.y += 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_DOWN))
+	//{
+	//	g_Fragment[2].overallPos.y -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_M))
+	//{
+	//	g_Fragment[2].overallPos.z -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_N))
+	//{
+	//	g_Fragment[2].overallPos.z += 1.0f;
+	//}
 
-	//猫の尻尾
-	/*if (GetKeyboardPress(DIK_LEFT))
-	{
-		g_Fragment[0].overallPos.x -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_RIGHT))
-	{
-		g_Fragment[0].overallPos.x += 1.0f;
-	}
-	if (GetKeyboardPress(DIK_UP))
-	{
-		g_Fragment[0].overallPos.y += 1.0f;
-	}
-	if (GetKeyboardPress(DIK_DOWN))
-	{
-		g_Fragment[0].overallPos.y -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_M))
-	{
-		g_Fragment[0].overallPos.z -= 1.0f;
-	}
-	if (GetKeyboardPress(DIK_N))
-	{
-		g_Fragment[0].overallPos.z += 1.0f;
-	}*/
+	////猫の尻尾
+	//if (GetKeyboardPress(DIK_LEFT))
+	//{
+	//	g_Fragment[0].overallPos.x -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_RIGHT))
+	//{
+	//	g_Fragment[0].overallPos.x += 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_UP))
+	//{
+	//	g_Fragment[0].overallPos.y += 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_DOWN))
+	//{
+	//	g_Fragment[0].overallPos.y -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_M))
+	//{
+	//	g_Fragment[0].overallPos.z -= 1.0f;
+	//}
+	//if (GetKeyboardPress(DIK_N))
+	//{
+	//	g_Fragment[0].overallPos.z += 1.0f;
+	//}
 
-	//猫の頭
-	if (GetKeyboardPress(DIK_O))
-	{
-		g_Fragment[0].scl.x -= 0.01f;
-	}
-	if (GetKeyboardPress(DIK_P))
-	{
-		g_Fragment[0].scl.x += 0.01f;
-	}
-	if (GetKeyboardPress(DIK_O))
-	{
-		g_Fragment[0].scl.y -= 0.01f;
-	}
-	if (GetKeyboardPress(DIK_P))
-	{
-		g_Fragment[0].scl.y += 0.01f;
-	}
+	////猫の頭
+	//if (GetKeyboardPress(DIK_O))
+	//{
+	//	g_Fragment[0].scl.x -= 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_P))
+	//{
+	//	g_Fragment[0].scl.x += 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_O))
+	//{
+	//	g_Fragment[0].scl.y -= 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_P))
+	//{
+	//	g_Fragment[0].scl.y += 0.01f;
+	//}
 
-	//猫の体
-	if (GetKeyboardPress(DIK_U))
-	{
-		g_Fragment[1].scl.x -= 0.01f;
-	}
-	if (GetKeyboardPress(DIK_I))
-	{
-		g_Fragment[1].scl.x += 0.01f;
-	}
-	if (GetKeyboardPress(DIK_U))
-	{
-		g_Fragment[1].scl.y -= 0.01f;
-	}
-	if (GetKeyboardPress(DIK_I))
-	{
-		g_Fragment[1].scl.y += 0.01f;
-	}
+	////猫の体
+	//if (GetKeyboardPress(DIK_U))
+	//{
+	//	g_Fragment[1].scl.x -= 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_I))
+	//{
+	//	g_Fragment[1].scl.x += 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_U))
+	//{
+	//	g_Fragment[1].scl.y -= 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_I))
+	//{
+	//	g_Fragment[1].scl.y += 0.01f;
+	//}
 
-	//猫の手
-	if (GetKeyboardPress(DIK_H))
-	{
-		g_Fragment[2].scl.x -= 0.01f;
-	}
-	if (GetKeyboardPress(DIK_J))
-	{
-		g_Fragment[2].scl.x += 0.01f;
-	}
-	if (GetKeyboardPress(DIK_H))
-	{
-		g_Fragment[2].scl.y -= 0.01f;
-	}
-	if (GetKeyboardPress(DIK_J))
-	{
-		g_Fragment[2].scl.y += 0.01f;
-	}
+	////猫の手
+	//if (GetKeyboardPress(DIK_H))
+	//{
+	//	g_Fragment[2].scl.x -= 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_J))
+	//{
+	//	g_Fragment[2].scl.x += 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_H))
+	//{
+	//	g_Fragment[2].scl.y -= 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_J))
+	//{
+	//	g_Fragment[2].scl.y += 0.01f;
+	//}
 
-	//猫の尻尾
-	if (GetKeyboardPress(DIK_K))
-	{
-		g_Fragment[3].scl.x -= 0.01f;
-	}
-	if (GetKeyboardPress(DIK_L))
-	{
-		g_Fragment[3].scl.x += 0.01f;
-	}
-	if (GetKeyboardPress(DIK_K))
-	{
-		g_Fragment[3].scl.y -= 0.01f;
-	}
-	if (GetKeyboardPress(DIK_L))
-	{
-		g_Fragment[3].scl.y += 0.01f;
-	}
+	////猫の尻尾
+	//if (GetKeyboardPress(DIK_K))
+	//{
+	//	g_Fragment[3].scl.x -= 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_L))
+	//{
+	//	g_Fragment[3].scl.x += 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_K))
+	//{
+	//	g_Fragment[3].scl.y -= 0.01f;
+	//}
+	//if (GetKeyboardPress(DIK_L))
+	//{
+	//	g_Fragment[3].scl.y += 0.01f;
+	//}
+
+//猫
+if (GetKeyboardPress(DIK_LEFT))
+{
+	g_Fragment[4].overallPos.x -= 1.0f;
+}
+if (GetKeyboardPress(DIK_RIGHT))
+{
+	g_Fragment[4].overallPos.x += 1.0f;
+}
+if (GetKeyboardPress(DIK_UP))
+{
+	g_Fragment[4].overallPos.y += 1.0f;
+}
+if (GetKeyboardPress(DIK_DOWN))
+{
+	g_Fragment[4].overallPos.y -= 1.0f;
+}
+if (GetKeyboardPress(DIK_M))
+{
+	g_Fragment[4].overallPos.z -= 1.0f;
+}
+if (GetKeyboardPress(DIK_N))
+{
+	g_Fragment[4].overallPos.z += 1.0f;
+}
 
 
 #endif
@@ -382,56 +453,81 @@ void DrawFragment(void)
 	/*for (int i = 0; i < g_Field.historyCount; ++i) {
 		XMMATRIX mtxTranslate = XMMatrixTranslation(g_Field.history[i].x, g_Field.history[i].y, g_Field.history[i].z);
 		SetWorldMatrix(&mtxTranslate);
-		GetDeviceContext()->Draw(4, 0); 
+		GetDeviceContext()->Draw(4, 0);
 	}*/
 
 	SetCullingMode(CULL_MODE_NONE);
 	SetLightEnable(FALSE);
 	SetDepthEnable(FALSE);
 
-	for (int i = 0; i < TEXTURE_MAX; i++)
+	if (!g_ShowFullImage)
+	{
+		for (int i = 0; i < TEXTURE_MAX; i++)
+		{
+
+			XMMATRIX mtxWorld = XMMatrixIdentity();
+			XMMATRIX mtxScl = XMMatrixScaling(g_Fragment[i].scl.x, g_Fragment[i].scl.y, g_Fragment[i].scl.z);
+			XMMATRIX mtxRot = XMMatrixRotationRollPitchYaw(g_Fragment[i].rot.x, g_Fragment[i].rot.y, g_Fragment[i].rot.z);
+			XMMATRIX mtxTranslate = XMMatrixTranslation(g_Fragment[i].overallPos.x, g_Fragment[i].overallPos.y + 60.0f, g_Fragment[i].overallPos.z + 200.0f);
+			mtxWorld = mtxScl * mtxRot * mtxTranslate;
+
+			/*g_Fragment[0].overallPos = XMFLOAT3(10.0f, 0.0f, 60.0f);
+			g_Fragment[1].overallPos = XMFLOAT3(30.0f, 0.0f, 40.0f);
+			g_Fragment[2].overallPos = XMFLOAT3(5.0f,  0.0f, 80.0f);
+			g_Fragment[3].overallPos = XMFLOAT3(70.0f, 0.0f, 90.0f);*/
+
+			//g_Fragment[4].overallPos = XMFLOAT3(70.0f, 0.0f, 90.0f);
+
+			g_Fragment[0].overallPos = XMFLOAT3(-513.0f, -4.0f, 402.0f);
+			g_Fragment[1].overallPos = XMFLOAT3(-511.0f, -4.0f, 291.0f);
+			g_Fragment[2].overallPos = XMFLOAT3(-518.0f, -4.0f, 280.0f);
+			g_Fragment[3].overallPos = XMFLOAT3(-518.0f, -5.0f, 223.0f);
+
+
+			g_Fragment[0].scl = XMFLOAT3(1.21f, 1.21f, 0.0f);
+			g_Fragment[1].scl = XMFLOAT3(0.87f, 0.87f, 0.0f);
+			g_Fragment[2].scl = XMFLOAT3(0.84f, 0.84f, 0.0f);
+			g_Fragment[3].scl = XMFLOAT3(0.9f, 0.9f, 0.0f);
+
+
+			/*g_Fragment[0].scl = XMFLOAT3(0.0f, 0.0f, 0.0f);
+			g_Fragment[1].scl = XMFLOAT3(0.0f, 0.0f, 0.0f);
+			g_Fragment[2].scl = XMFLOAT3(0.0f, 0.0f, 0.0f);
+			g_Fragment[3].scl = XMFLOAT3(0.0f, 0.0f, 0.0f);*/
+
+			// 猫の正しい見つかり位置座標
+			// -507 , 57 , 70 - 80 
+
+			// ワールドマトリックスの初期化
+			SetWorldMatrix(&mtxWorld);
+			XMStoreFloat4x4(&g_Fragment[i].mtxWorld, mtxWorld);
+
+			// テクスチャー設定
+			GetDeviceContext()->PSSetShaderResources(0, 1, &g_Texture[i]);
+
+			//ポリゴン描画
+			GetDeviceContext()->Draw(4, 0);
+		}
+	}
+	
+
+	if (g_ShowFullImage)
 	{
 		
+		XMFLOAT3 center = ComputePuzzleCenter();  // 或者指定碎片坐标
+		XMFLOAT3 scale = ComputeAverageScale();
+
+
 		XMMATRIX mtxWorld = XMMatrixIdentity();
-		XMMATRIX mtxScl = XMMatrixScaling(g_Fragment[i].scl.x, g_Fragment[i].scl.y, g_Fragment[i].scl.z);
-		XMMATRIX mtxRot = XMMatrixRotationRollPitchYaw(g_Fragment[i].rot.x, g_Fragment[i].rot.y, g_Fragment[i].rot.z);
-		XMMATRIX mtxTranslate = XMMatrixTranslation(g_Fragment[i].overallPos.x, g_Fragment[i].overallPos.y + 60.0f, g_Fragment[i].overallPos.z+200.0f);
-		mtxWorld = mtxScl * mtxRot * mtxTranslate;
+		XMMATRIX mtxScl = XMMatrixScaling(scale.x, scale.y, scale.z);  // 你可以调整缩放
+		XMMATRIX mtxTranslate = XMMatrixTranslation(center.x, center.y + 60.0f, center.z + 200.0f);
+		mtxWorld = mtxScl * mtxTranslate;
 
-		/*g_Fragment[0].overallPos = XMFLOAT3(10.0f, 0.0f, 60.0f);
-		g_Fragment[1].overallPos = XMFLOAT3(30.0f, 0.0f, 40.0f);
-		g_Fragment[2].overallPos = XMFLOAT3(5.0f,  0.0f, 80.0f);
-		g_Fragment[3].overallPos = XMFLOAT3(70.0f, 0.0f, 90.0f);*/
-
-		g_Fragment[0].overallPos = XMFLOAT3(-513.0f, -4.0f, 402.0f);
-		g_Fragment[1].overallPos = XMFLOAT3(-511.0f, -4.0f, 291.0f);
-		g_Fragment[2].overallPos = XMFLOAT3(-518.0f, -4.0f, 280.0f);
-		g_Fragment[3].overallPos = XMFLOAT3(-518.0f, -5.0f, 223.0f);
-
-
-		g_Fragment[0].scl = XMFLOAT3(1.21f, 1.21f, 0.0f);
-		g_Fragment[1].scl = XMFLOAT3(0.87f, 0.87f, 0.0f);
-		g_Fragment[2].scl = XMFLOAT3(0.84f, 0.84f, 0.0f);
-		g_Fragment[3].scl = XMFLOAT3(0.9f, 0.9f, 0.0f);
-
-
-		/*g_Fragment[0].scl = XMFLOAT3(0.0f, 0.0f, 0.0f);
-		g_Fragment[1].scl = XMFLOAT3(0.0f, 0.0f, 0.0f);
-		g_Fragment[2].scl = XMFLOAT3(0.0f, 0.0f, 0.0f);
-		g_Fragment[3].scl = XMFLOAT3(0.0f, 0.0f, 0.0f);*/
-
-		// 猫の正しい見つかり位置座標
-		// -507 , 57 , 70 - 80 
-
-		// ワールドマトリックスの初期化
 		SetWorldMatrix(&mtxWorld);
-		XMStoreFloat4x4(&g_Fragment[i].mtxWorld, mtxWorld);
+		GetDeviceContext()->PSSetShaderResources(0, 1, &g_Texture[4]);  // 最后一张完整图
 
-		// テクスチャー設定
-		GetDeviceContext()->PSSetShaderResources(0, 1, &g_Texture[i]);
-
-		//ポリゴン描画
-		GetDeviceContext()->Draw(4, 0);  
+		GetDeviceContext()->Draw(4, 0);
+		
 	}
 
 	SetLightEnable(TRUE);
